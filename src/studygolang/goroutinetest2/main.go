4e521b7afb0a6966ad7e1a624c31162c8fadf624
weizili.360.laptop <zieckey@gmail.com>
2014-11-06 17:30:56 +0800

    add goroutinetest 2

---
+++ src/studygolang/goroutinetest2/main.go
@@ -0,0 +1,16 @@
+package goroutinetest2
+
+import "os"
+
+func main() {
+    for i:=0; i<20; i++ {
+        go func() {
+            for {
+                b:=make([]byte, 10)
+                os.Stdin.Read(b) // will block
+            }
+        }()
+    }
+
+    select{}
+}
