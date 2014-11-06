26fd4bcc4d7aeb8ea8e9665cbe798f4690c87e94
weizili.360.laptop <zieckey@gmail.com>
2014-11-06 15:23:42 +0800

    modify goroutine test

--- src/studygolang/goroutinetest1/main.go
+++ src/studygolang/goroutinetest1/main.go
@@ -9,17 +9,23 @@ func main() {
 	ch := make(chan int)     
 	task("A", ch)     
 	task("B", ch)     
+	task("C", ch)     
+	task("D", ch)     
 	fmt.Printf("begin\n")     
 	<-ch     
-	<-ch 
-	
-} 
+	<-ch
+	<-ch     
+	<-ch
+}
 
 func task(name string, ch chan int) {     
 	go func() {         
 		i:= 1         
 		for {             
-			//fmt.Printf("%s %s\n", name, strconv.Itoa(i))             
+			//fmt.Printf("%s %s\n", name, strconv.Itoa(i)) 
+			for j:=0;j<1000000;j++ {
+				strconv.Itoa(i) // cost some CPU time
+			}           
 			print(name + " " + strconv.Itoa(i) + "\n")
 			i++         
 		}         
