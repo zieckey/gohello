db3bd7eba5bb594416aa07be8d58797318153140
weizili.build8 <zieckey@gmail.com>
2014-11-06 16:01:21 +0800

    Add runtime.GOMAXPROCS

--- src/studygolang/goroutinetest1/main.go
+++ src/studygolang/goroutinetest1/main.go
@@ -1,36 +1,39 @@
-package main 
+package main
 
-import (     
+import (
 	"fmt"
-	"strconv" 
-) 
+	"strconv"
+    "runtime"
+)
 
-func main() {     
-	ch := make(chan int)     
-	task("A", ch)     
-	task("B", ch)     
-	task("C", ch)     
-	task("D", ch)     
-	fmt.Printf("begin\n")     
-	<-ch     
+func main() {
+    //runtime.GOMAXPROCS(runtime.NumCPU())
+    runtime.GOMAXPROCS(2)
+	ch := make(chan int)
+	task("A", ch)
+	task("B", ch)
+	task("C", ch)
+	task("D", ch)
+	fmt.Printf("begin\n")
 	<-ch
-	<-ch     
+	<-ch
+	<-ch
 	<-ch
 }
 
-func task(name string, ch chan int) {     
-	go func() {         
-		i:= 1         
-		for {             
+func task(name string, ch chan int) {
+	go func() {
+		i:= 1
+		for {
 			//fmt.Printf("%s %s\n", name, strconv.Itoa(i)) 
 			for j:=0;j<1000000;j++ {
 				strconv.Itoa(i) // cost some CPU time
-			}           
+			}
 			print(name + " " + strconv.Itoa(i) + "\n")
-			i++         
-		}         
-		ch <- 1     
-	}(); 
+			i++
+		}
+		ch <- 1
+	}();
 }
 
 
