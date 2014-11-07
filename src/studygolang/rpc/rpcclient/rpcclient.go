178a379280a3e0ffcd3c6ea95559dfd68fae8254
weizili.360.laptop <zieckey@gmail.com>
2014-11-07 10:07:40 +0800

    Add rpc test code

---
+++ src/studygolang/rpc/rpcclient/rpcclient.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+  "net/rpc"
+  "fmt"
+  "log"
+  "os"
+)
+
+type Args struct {
+  A,B int
+}
+
+type Quotient struct {
+  Quo,Rem int
+}
+
+func main() {
+  if len(os.Args) != 2 {
+    fmt.Println("Usage: ",os.Args[0],"server")
+	os.Exit(1)
+  }
+  serverAddress := os.Args[1]
+  
+  client,err := rpc.DialHTTP("tcp", serverAddress+":1234")
+  if err != nil {
+    log.Fatal("dialing:",err)
+  }
+  
+  // Synchronous call
+  args := Args{17,8}
+  var reply int
+  //远程调用，args是传给远程函数的参数，reply用来接收函数的结果
+  err = client.Call("Arith.Multiply",args,&reply)
+  if err != nil {
+    log.Fatal("arith error:",err)
+  }
+  fmt.Printf("Arith: %d*%d=%d\n",args.A,args.B,reply)
+
+  var quot Quotient
+  //远程调用，args是传给远程函数的参数，reply用来接收函数的结果
+  err = client.Call("Arith.Divide",args, &quot)
+  if err != nil {
+    log.Fatal("arith error:",err)
+  }
+  fmt.Printf("Arith: %d/%d=%d remainder %d\n",args.A,args.B,quot.Quo,quot.Rem)
+}
---
+++ src/studygolang/rpc/rpcserver/rpcserver.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+  "fmt"
+  "net/rpc"
+  "errors"
+  "net/http"
+)
+
+type Args struct {
+  A,B int
+}
+
+type Quotient struct {
+  Quo,Rem int
+}
+
+type Arith int
+
+// 需要远程调用的方法
+func (t *Arith) Multiply(args *Args, reply *int) error {
+  *reply = args.A * args.B
+  fmt.Printf("Multiply : %v * %v = %v\n",  args.A, args.B, *reply)
+  return nil
+}
+// 需要远程调用的方法
+func (t *Arith) Divide(args *Args,quo *Quotient) error {
+  if args.B == 0 {
+    return errors.New("divide by zero")
+  }
+  quo.Quo = args.A / args.B
+  quo.Rem = args.A % args.B
+  fmt.Printf("Divide : %v / %v = %v|%v\n",  args.A, args.B, quo.Quo, quo.Rem)
+  return nil
+}
+
+func main() {
+  arith := new(Arith)
+  // 注册
+  rpc.Register(arith)
+  rpc.HandleHTTP()
+  
+  err := http.ListenAndServe(":1234",nil)
+  if err != nil {
+    fmt.Println(err.Error())
+  }
+}
