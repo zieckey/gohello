fe1107cf95416559eaabac2805746a3ecac019cc
weizili.build8 <zieckey@gmail.com>
2014-11-11 17:21:03 +0800

    rm cgo and protobuf. we move it to branch cgo

--- src/cgohello/myprint.c
+++
@@ -1,7 +0,0 @@
-#include "myprint.h"
-#include <stdio.h>
-
-void myprint(const char* str)
-{
-  printf("%s\n", str);
-}
--- src/cgohello/myprint.h
+++
@@ -1,4 +0,0 @@
-#ifndef PRINTS_HEAD
-#define PRINTS_HEAD
-void myprint(const char* str);
-#endif
