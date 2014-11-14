253a43fc0230697129822795ec7d1d3279d21863
weizili.360.laptop <zieckey@gmail.com>
2014-11-14 18:53:25 +0800

    add map.go test code

---
+++ studygolang/pkg/map.go
@@ -0,0 +1,38 @@
+package pkg
+
+import (
+	"fmt"
+)
+
+func TestMap() {
+	type Kvmap map[string]string
+	type SectionMap map[string]Kvmap
+
+	m := make(Kvmap)
+	m["a"] = "av"
+	m["b"] = "bv"
+	fmt.Printf("m: %v\n", m)
+
+	fmt.Printf("===> Assign m -> s\n")
+	s := make(SectionMap)
+	s[""] = m	// 是引用的拷贝，map本身的内容都同一份
+	s[""]["s"] = "sv"
+	m["c"] = "cv"
+	fmt.Printf("m: %v\n", m)
+	sd, _ := s[""]
+	fmt.Printf("s[\"\"]: %v\n", sd)
+
+	fmt.Printf("===> Assign another m -> s\n")
+	m = make(Kvmap)
+	s["m"] = m
+	s["m"]["s"] = "nnnsv"
+	m["c"] = "nnncv"
+	fmt.Printf("m: %v\n", m)
+	sd, _ = s["m"]
+	fmt.Printf("s[\"m\"]: %v\n", sd)
+	fmt.Printf("s: %v\n", s)
+	
+	
+	v,ok := m["c"]
+	fmt.Printf("the value of [c] in m is [%v] ok=%v", v,ok)
+}
--- studygolang/study.go
+++ studygolang/study.go
@@ -29,6 +29,7 @@ func main() {
 	pkg.TestJSON()
 	pkg.TestGoSimpleJSON()
 	pkg.TestExec_date()
+	pkg.TestMap()
 }
 
 
@@ -145,3 +146,30 @@ func TestPanicRecover() {
 	IntFromInt64(1099511627775)
 	IntFromInt64(2)
 }
+
+
+/*
+package main
+ 
+import (
+  "fmt"
+  "testing"
+)
+ 
+func assertEqual(t *testing.T, a interface{}, b interface{}, message string) {
+  if a == b {
+    return
+	}
+	if len(message) == 0 {
+		message = fmt.Sprintf("%v != %v", a, b)
+	}
+	t.Fatal(message)
+}
+ 
+func TestSimple(t *testing.T) {
+  a := 42
+  assertEqual(t, a, 42, "")
+  assertEqual(t, a, 43, "This message is displayed in place of the default one")
+}
+
+*/
