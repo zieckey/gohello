4088f44fb0983acc49b021f3744c340f481f48ee
weizili.360.laptop <zieckey@gmail.com>
2014-11-17 21:30:16 +0800

    Add comments of json unicode test code

--- studygolang/unicodejsontest/main.go
+++ studygolang/unicodejsontest/main.go
@@ -11,19 +11,21 @@ echo json_encode(json_decode($s));
 // It output : {"Name":"\u9b4f"}
 */
 
+// Go Playground : http://play.golang.org/p/D9uQlFgPJo
+
 func main() {
-		fmt.Printf("\n\n")
+	fmt.Printf("\n\n")
 	var s = make(map[string]interface{})
 	s["Name"] = "魏"
 	result, _ := json.Marshal(s)
 	fmt.Println("json.Marshal ==> ", string(result))
-	
+
 	type User struct {
 		Name string
 	}
-	
+
 	u := &User{}
 	_ = json.Unmarshal(result, u)
 	fmt.Printf("json.Unmarshal ==> Name=%v\n", u.Name)
-}
 
+}
