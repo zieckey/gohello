b022c66fd5884b53d506be35510a76468f252be4
weizili.360.laptop <zieckey@gmail.com>
2014-11-24 18:29:18 +0800

    add channel test code

---
+++ studygolang/channel/channel1/chennel1.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+        "fmt"
+        "sync"
+        "time"
+)
+
+func main() {
+        finish := make(chan bool)
+        var done sync.WaitGroup
+        done.Add(1)
+        go func() {
+                select {
+                case <-time.After(1 * time.Hour):
+                case <-finish:
+                }
+                done.Done()
+        }()
+        t0 := time.Now()
+        finish <- true // 发送关闭信号
+        done.Wait()    // 等待 goroutine 结束
+        fmt.Printf("Waited %v for goroutine to stop\n", time.Since(t0))
+}
