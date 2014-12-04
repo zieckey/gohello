492c199492b2256f18c2891c218dfff90dbb8792
weizili.360.laptop <zieckey@gmail.com>
2014-12-04 17:09:44 +0800

    Fix http echo server bug

--- benchmark/httpecho/main.go
+++ benchmark/httpecho/main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"log"
 	"net/http"
+	"io/ioutil"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	buf := make([]byte, 1024)
-	n, _ := r.Body.Read(buf) //Read the http body
-	w.Write(buf[0:n])
+	buf, err := ioutil.ReadAll(r.Body) //Read the http body
+	if err != nil {
+		w.Write(buf)
+		return
+	}
+
+	w.WriteHeader(403)
 }
 
 func main() {
