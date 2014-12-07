2bcd8d7087ec241fa6fad1046c3fa949e7a8d14f
weizili.360.laptop <zieckey@gmail.com>
2014-12-07 17:45:22 +0800

    Add debug log

--- studygolang/httpprint/main.go
+++ studygolang/httpprint/main.go
@@ -12,20 +12,21 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	for {
 		n, err := r.Body.Read(buf)
 		sum += n
+		log.Printf("recv %v byte, sum=%v, err=%v\n", n, sum, err.Error())
 		if sum >= int(len) {
 			break
 		}
 		if err != nil {
-			log.Printf("recv %v byte, ERROR %v\n", n, err.Error())
+			log.Printf("recv %v byte, sum=%v, err=%v\n", n, sum, err.Error())
 			break
 		}
-		log.Printf("recv %v byte\n", n)
 	}
 
 	w.Write([]byte("OK"))
 }
 
 func main() {
+	log.SetFlags(log.Lshortfile | log.Ldate | log.Ltime)
 	http.HandleFunc("/echo", handler)
 	log.Fatal(http.ListenAndServe(":8091", nil))
 }
