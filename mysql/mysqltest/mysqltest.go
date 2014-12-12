6407bb875d48c65a7da8fcafac23de6322144ed0
weizili.360.laptop <zieckey@gmail.com>
2014-12-12 17:51:48 +0800

    add mysql test code

---
+++ mysql/mysqltest/mysqltest.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:@/test")
+	if err != nil {
+		panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
+	}
+	defer db.Close()
+
+	// Open doesn't open a connection. Validate DSN data:
+	err = db.Ping()
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+
+}
