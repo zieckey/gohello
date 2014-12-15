89882419191dea9052903cc93892ba6db662ef9f
weizili.360.laptop <zieckey@gmail.com>
2014-12-15 12:02:12 +0800

    delete cgo relative code. moved it to go1.3 branch

--- studygolang/goid/goid.c
+++
@@ -1,6 +0,0 @@
-#include "runtime.h"
-
-void ·Get(int32 ret) {
-	ret = g->goid;
-	USED(&ret);
-}
--- studygolang/goid/goid.go
+++
@@ -1,6 +0,0 @@
-// Copy from https://github.com/funny/goid
-
-package goid
-
-// Get current goroutine id.
-func Get() int32
