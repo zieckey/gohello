63d2563040eedd431ede299b17f701b9cc61197f
weizili.360.laptop <zieckey@gmail.com>
2014-12-19 17:37:39 +0800

    refactor stackfunc_goid

---
+++ goroutineid/stackfunc_goid/goid.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"runtime"
+	"bytes"
+)
+
+// Take it from : http://camlistore.org/pkg/syncutil/lock.go?s=821:1134
+
+const stackBufSize = 16 << 20
+
+var stackBuf = make(chan []byte, 8)
+
+func getBuf() []byte {
+	select {
+	case b := <-stackBuf:
+		return b[:stackBufSize]
+	default:
+		return make([]byte, stackBufSize)
+	}
+}
+
+func putBuf(b []byte) {
+	select {
+	case stackBuf <- b:
+	default:
+	}
+}
+
+var goroutineSpace = []byte("goroutine ")
+
+func GoroutineID() int64 {
+	b := getBuf()
+	defer putBuf(b)
+	b = b[:runtime.Stack(b, false)]
+	// Parse the 4707 out of "goroutine 4707 [running]:"
+	fmt.Printf("STACK BEGIN:\n[%s]\nSTACK END\n", string(b))
+	b = bytes.TrimPrefix(b, goroutineSpace)
+	i := bytes.IndexByte(b, ' ')
+	if i < 0 {
+		panic(fmt.Sprintf("No space found in %q", b))
+	}
+	b = b[:i]
+	n, err := strconv.ParseInt(string(b), 10, 64)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to parse goroutine ID out of %q: %v", b, err))
+	}
+	return int64(n)
+}
--- goroutineid/stackfunc_goid/main.go
+++ goroutineid/stackfunc_goid/main.go
@@ -4,54 +4,16 @@ import (
 	"fmt"
 	"strconv"
 	"runtime"
-	"bytes"
 )
 
-const stackBufSize = 16 << 20
-
-var stackBuf = make(chan []byte, 8)
-
-func getBuf() []byte {
-	select {
-	case b := <-stackBuf:
-		return b[:stackBufSize]
-	default:
-		return make([]byte, stackBufSize)
-	}
-}
-
-func putBuf(b []byte) {
-	select {
-	case stackBuf <- b:
-	default:
-	}
-}
-
-var goroutineSpace = []byte("goroutine ")
-
-func GoroutineID() int64 {
-	b := getBuf()
-	defer putBuf(b)
-	b = b[:runtime.Stack(b, false)]
-	// Parse the 4707 otu of "goroutine 4707 ["
-	b = bytes.TrimPrefix(b, goroutineSpace)
-	i := bytes.IndexByte(b, ' ')
-	if i < 0 {
-		panic(fmt.Sprintf("No space found in %q", b))
-	}
-	b = b[:i]
-	n, err := strconv.ParseInt(string(b), 10, 64)
-	if err != nil {
-		panic(fmt.Sprintf("Failed to parse goroutine ID out of %q: %v", b, err))
-	}
-	return int64(n)
-}
 
 func main() {
-
+    //runtime.GOMAXPROCS(runtime.NumCPU())
+    runtime.GOMAXPROCS(2)
     ch := make(chan int)
     fmt.Printf("begin\n")
-    for j := 0; j < 1000; j++ {
+    loop := 10
+    for j := 0; j < loop; j++ {
 	    task("A" + strconv.Itoa(j), ch)
     }
     <-ch
@@ -66,7 +28,7 @@ func task(name string, ch chan int) {
             for j := 0; j < 20; j++ {
                 strconv.Itoa(i) // cost some CPU time
             }
-            print("goid=" + strconv.Itoa(int(GoroutineID())) + " " + name + " " + strconv.Itoa(i) + "\n")
+            fmt.Printf("goid=" + strconv.Itoa(int(GoroutineID())) + " " + name + " " + strconv.Itoa(i) + "\n")
             i++
         }
         ch <- 1
