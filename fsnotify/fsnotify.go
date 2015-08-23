857b1a517f9fbe1eded7705860c9c38a372e1bff
weizili.laptop <zieckey@gmail.com>
2015-08-23 19:00:55 +0800

    Add fsnotify test code

---
+++ fsnotify/fsnotify.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"log"
+
+	"github.com/howeyc/fsnotify"
+)
+
+func main() {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	done := make(chan bool)
+
+	// Process events
+	go func() {
+		for {
+			select {
+			case ev := <-watcher.Event:
+				log.Println("event:", ev)
+			case err := <-watcher.Error:
+				log.Println("error:", err)
+			}
+		}
+	}()
+
+	//err = watcher.Watch("e:/1/a.txt")
+	err = watcher.Watch("/tmp/a.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	<-done
+
+	/* ... do stuff ... */
+	watcher.Close()
+}
