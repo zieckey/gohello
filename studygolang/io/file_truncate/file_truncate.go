a2e549dc34e7c1fbee691df6e2cb48cb5f0eca18
weizili.laptop <zieckey@gmail.com>
2015-08-29 17:19:34 +0800

    Add file truncating test code

---
+++ studygolang/io/file_truncate/file_truncate.go
@@ -0,0 +1,22 @@
+package main
+import (
+    "os"
+    "strconv"
+    "time"
+    "flag"
+)
+
+func main() {
+    var trunclen *int = flag.Int("t", -1, "The length of truncating")
+    flag.Parse()
+    fp, _ := os.OpenFile("test_truncate." + strconv.FormatInt(time.Now().UnixNano(), 10) + ".txt", os.O_CREATE | os.O_RDWR, 0755)
+    fp.WriteString("1234567890\n")
+    fp.WriteString("abcdefghij\n")
+
+    if *trunclen >= 0 {
+        fp.Truncate(int64(*trunclen))
+    }
+
+
+    fp.Close()
+}
