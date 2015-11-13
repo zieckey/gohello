dffc6844135cc8e3ee043f01b013d54aa0105991
weizili.laptop <zieckey@gmail.com>
2015-11-13 14:41:54 +0800

    up exec

--- net/sendmail/main.go
+++ net/sendmail/main.go
@@ -60,17 +60,64 @@ func main() {
     c.Quit()
 }*/
 
+//package main
+//import (
+//    "os/exec"
+//    "fmt"
+//    "log"
+//)
+//
+//func main() {
+//    message := "hello, this is the email body"
+//    title := "email title ttt"
+//    cmd := fmt.Sprintf("echo '%v' | mail -s ‘%v’ weizili@360.cn", message, title)
+//    c := exec.Command(cmd)
+//    log.Printf(c.Run())
+//}
+
+
 package main
+
 import (
-    "os/exec"
     "fmt"
-    "log"
+    "github.com/mattbaird/gochimp"
 )
 
 func main() {
-    message := "hello, this is the email body"
-    title := "email title ttt"
-    cmd := fmt.Sprintf("echo '%v' | mail -s ‘%v’ weizili@360.cn", message, title)
-    c := exec.Command(cmd)
-    log.Printf(c.Run())
+    //apiKey := os.Getenv("MANDRILL_KEY")
+    apiKey := "111111111-1111-1111-1111-111111111"
+    mandrillApi, err := gochimp.NewMandrill(apiKey)
+
+    if err != nil {
+        fmt.Println("Error instantiating client")
+    }
+
+    templateName := "welcome email"
+    contentVar := gochimp.Var{"main", "<h1>Welcome aboard</h1>"}
+    content := []gochimp.Var{contentVar}
+
+    renderedTemplate, err := mandrillApi.TemplateRender(templateName, content, nil)
+
+    if err != nil {
+        fmt.Println("Error rendering template:", err.Error())
+        return
+    }
+
+    recipients := []gochimp.Recipient{
+        gochimp.Recipient{Email: "zieckey@163.com"},
+    }
+
+    message := gochimp.Message{
+        Html:      renderedTemplate,
+        Subject:   "Welcome aboard!",
+        FromEmail: "bossman@example.com",
+        FromName:  "Boss Man",
+        To:        recipients,
+    }
+
+    _, err = mandrillApi.MessageSend(message, false)
+
+    if err != nil {
+        fmt.Println("Error sending message:", err.Error())
+    }
 }
---
+++ os/exec/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "os/exec"
+    "fmt"
+    "os"
+)
+
+func main() {
+    message := "hello, this is the email body"
+    title := "email title ttt"
+    c := exec.Command("echo", message, "|", "mail", "-s", title, "weizili@360.cn")
+    buf, err := c.Output()
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "The command failed to perform: %s (Command: %s, Arguments: %s)", err, c.Args)
+        return
+    }
+    fmt.Fprintf(os.Stdout, "Result: %s", buf)
+}
