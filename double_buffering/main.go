4840ae873a40412cffeb2554f67801a986521220
weizili.laptop <zieckey@gmail.com>
2016-01-17 21:16:53 +0800

    Add double_buffering.go and test OK

--- double_buffering/main.go
+++ double_buffering/main.go
@@ -11,12 +11,26 @@ import (
 	"io"
 )
 
-var blackIDs map[string]int
-func LoadBlackIDs(filepath string) error {
+type BlackIDDict struct {
+	blackIDs map[string]int
+}
+
+func NewBlackIDDict() DoubleBufferingTarget {
+	d := &BlackIDDict{
+		blackIDs: make(map[string]int),
+	}
+	return d
+}
+
+var dbm *DoubleBufferingManager
+
+func (d *BlackIDDict) Initialize(conf string) bool {
+	filepath := conf
+
 	// 加载黑名单列表文件，每行一个
 	b, err := ioutil.ReadFile(filepath)
 	if err != nil {
-		return err
+		return false
 	}
 	r := bytes.NewBuffer(b)
 	for {
@@ -24,7 +38,7 @@ func LoadBlackIDs(filepath string) error {
 		if err == io.EOF || err == nil {
 			id = strings.TrimSpace(id)
 			if len(id) > 0 {
-				blackIDs[id] = 1
+				d.blackIDs[id] = 1
 			}
 		}
 
@@ -33,11 +47,16 @@ func LoadBlackIDs(filepath string) error {
 		}
 	}
 
-	return nil
+	return true
 }
 
-func IsBlackID(id string) bool {
-	_, exist := blackIDs[id]
+func (d *BlackIDDict) Close() {
+	// 在这里做一些资源释放工作
+	// 当前这个例子没有资源需要我们手工释放
+}
+
+func (d *BlackIDDict) IsBlackID(id string) bool {
+	_, exist := d.blackIDs[id]
 	return exist
 }
 
@@ -45,9 +64,17 @@ func Query(r *http.Request) (string, error) {
 	id := r.FormValue("id")
 	query := r.FormValue("query")
 
-	//参数合法性检查
+	//TODO 参数合法性检查
 
-	if IsBlackID(id) {
+	d := dbm.Get("black_id")
+	tg := d.Get()
+	defer tg.Release()
+	dict := tg.Target.(*BlackIDDict)  // 转换为具体的Dict对象
+	if dict == nil {
+		return "", fmt.Errorf("ERROR, Convert DoubleBufferingTarget to Dict failed")
+	}
+
+	if dict.IsBlackID(id) {
 		return "ERROR", fmt.Errorf("ERROR id")
 	}
 
@@ -72,16 +99,29 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func Reload(w http.ResponseWriter, r *http.Request) {
+	// 这里简化处理，直接重新加载black_id。如果有多个，可以从url参数中获取资源名称
+	if dbm.Reload("black_id", os.Args[1]) {
+		w.Write([]byte("OK"))
+	} else {
+		w.Write([]byte("FAILED"))
+	}
+}
+
 func main() {
-	blackIDs = make(map[string]int)
-	if len(os.Args) == 2 {
-		err := LoadBlackIDs(os.Args[1])
-		panic(err)
+	if len(os.Args) != 2 {
+		panic("Not specify black_id.txt")
+	}
+
+	dbm = NewDoubleBufferingManager()
+	rc := dbm.Add("black_id", os.Args[1], NewBlackIDDict)
+	if rc == false {
+		panic("black_id initialize failed")
 	}
 
 	http.HandleFunc("/q", Handler)
+	http.HandleFunc("/admin/reload", Reload) // 管理接口，用于重新加载black_id.txt。如果有多个这种资源，可以增加一些参数来说区分不同的资源
 	hostname, _ := os.Hostname()
 	log.Printf("start http://%s:8091/q", hostname)
 	log.Fatal(http.ListenAndServe(":8091", nil))
 }
-
