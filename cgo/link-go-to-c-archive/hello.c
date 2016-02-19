4c54d17dcf8d56abccceefe28da66809b9a880f4
weizili.build9 <zieckey@gmail.com>
2016-02-19 16:23:47 +0800

    Add hello.c

---
+++ cgo/link-go-to-c-archive/hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "libhello.h"
+
+int main() {
+  printf("This is a C Application.\n");
+  GoString name = {(char*)"Jane", 4};
+  SayHello(name);
+  GoSlice buf = {(void*)"Jane", 4, 4};
+  SayHelloByte(buf);
+  SayBye();
+  return 0;
+}
--- cgo/link-go-to-c-archive/hello.go
+++ cgo/link-go-to-c-archive/hello.go
@@ -1,4 +1,3 @@
-// package name: nautilus
 package main
 
 import "C"
@@ -16,10 +15,24 @@ func SayHelloByte(name []byte) {
 
 //export SayBye
 func SayBye() {
-	fmt.Println("func in Golang SayBye says: Bye!\n")
+	fmt.Println("func in Golang SayBye says: Bye!")
 }
 
 func main() {
 	// We need the main function to make possible
 	// CGO compiler to compile the package as C shared library
+
+	/*
+		-buildmode=c-archive
+			Build the listed main package, plus all packages it imports,
+			into a C archive file. The only callable symbols will be those
+			functions exported using a cgo //export comment. Requires
+			exactly one main package to be listed.
+
+		-buildmode=c-shared
+			Build the listed main packages, plus all packages that they
+			import, into C shared libraries. The only callable symbols will
+			be those functions exported using a cgo //export comment.
+			Non-main packages are ignored.
+	*/
 }
