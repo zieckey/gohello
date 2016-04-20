a1750412a9fdb56ac3227289f0c6b6a8d2264572
weizili.build9 <zieckey@gmail.com>
2016-04-20 11:14:53 +0800

    update myprint.c

--- cgo/retptr/myprint.c
+++ cgo/retptr/myprint.c
@@ -7,7 +7,11 @@ char* retmalloc(int len, int *retlen)
 {
     static const char* s = "0123456789";
     char* p = malloc(len);
-    memcpy(p, s, len);
+    if (len <= strlen(s)) {
+        memcpy(p, s, len);
+    } else {
+        memset(p, 'a', len);
+    }
     *retlen = len;
     return p;
 }
