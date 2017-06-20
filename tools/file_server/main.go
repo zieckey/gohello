5ca152eb3813872a14d0a628805438cd93d57f6b
weizili <zieckey@gmail.com>
2017-06-20 20:31:11 +0800

    Add more debug log and add c10m tcp client

--- tools/file_server/main.go
+++ tools/file_server/main.go
@@ -8,8 +8,13 @@ import (
 func main() {
     dir := "."
     if len(os.Args) == 2 {
+        if (os.Args[1] == "--help" || os.Args[1] == "-help" || os.Args[1] == "-h") {
+            println("Usage: %v <the-path-to-dir>", os.Args[0])
+            return
+        }
         dir = os.Args[1]
     }
+    println("Start file server at http://127.0.0.1:8000/")
     http.Handle("/", http.FileServer(http.Dir(dir)))
     http.ListenAndServe(":8000", nil)
 }
