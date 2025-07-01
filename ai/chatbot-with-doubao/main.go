f780e2388befc82ec36305dc6b27957a62e68689
weizili <zieckey@gmail.com>
2025-07-01 17:21:53 +0800

    fix warning

--- ai/chatbot-with-doubao/main.go
+++ ai/chatbot-with-doubao/main.go
@@ -18,7 +18,7 @@ func main() {
 	// 创建一个上下文，通常用于传递请求的上下文信息，如超时、取消等
 	ctx := context.Background()
 	// 构建聊天完成请求，设置请求的模型和消息内容
-	req := model.ChatCompletionRequest{
+	req := model.CreateChatCompletionRequest{
 		// 将推理接入点 <Model>替换为 Model ID
 		Model: "doubao-seed-1-6-250615",
 		Messages: []*model.ChatCompletionMessage{
@@ -26,7 +26,7 @@ func main() {
 				// 消息的角色为用户
 				Role: model.ChatMessageRoleUser,
 				Content: &model.ChatCompletionMessageContent{
-					StringValue: volcengine.String("你好"),
+					StringValue: volcengine.String("你好，请分析下明太祖朱元璋的生平，从当时人民的生活水平到后世对他的评价，完整的评价一下他的一生，并且用中文回复"),
 				},
 			},
 		},
